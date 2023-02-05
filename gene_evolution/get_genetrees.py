9672adedacc4ff1fe66f0b889668c0aa250045df
Riccardo Kyriacou <99962616+RiccardoKyriacou@users.noreply.github.com>
2023-02-05 19:03:56 +0000

    Add files via upload
    Added binary tree output

--- gene_evolution/get_genetrees.py
+++ gene_evolution/get_genetrees.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
+import os
+import re
 import glob
 import shutil
 import argparse
 from Bio import SeqIO
 from Bio.Seq import Seq
+from collections import defaultdict
 from subprocess import call as unix
 
 '''
-This script first moves only OGs which contain 
-a high GC3 gene (i.e. genes in heatmap) into 
-a new directiory. Script will then perform 
-trimal alignment before running IQ tree on
-each OG file, creating gene trees. 
+This script:
+1) first moves only OGs which contain a high GC3 gene (i.e. genes in heatmap) into a new directiory. 
+2) Performs trimal alignment 
+3) Runs IQ tree on each OG file, creating gene trees.
+4) Finally, script will create binary tree files needed for RER converge
 '''
 
 # Usage: python3 get_genetrees.py --path ~/GC_analysis/GC_Lep/GC_cds_Lep/results/GC3/1to1_orthologues/outlier_GC3-comparison.tsv --out ../data/1to1_highGC3_OGs/ --species_tree ../data/Species_Tree/SpeciesTree_rooted_node_labels.txt
 
 parse = argparse.ArgumentParser()
 
-parse.add_argument("--path",type=str, help="path to outlier_GC3-comparison.tsv file to pull relevant OGS", required = True)
-parse.add_argument("--out",type=str, help="path to output for OGs", required = True)
-parse.add_argument("--species_tree",type=str, help="path to orthofinder species tree", required = True)
+parse.add_argument("-p", "--path",type=str, help="path to outlier_GC3-comparison.tsv file to pull relevant OGS", required = True)
+parse.add_argument("-o", "--out",type=str, help="path to output for OGs", required = True)
+parse.add_argument("-s", "--species_tree",type=str, help="path to orthofinder species tree", required = True)
 
 args = parse.parse_args()
 
@@ -85,24 +88,101 @@ def run_gene_iqtree(trimal_files, species_tree):
     for alignment in glob.glob(trimal_files + "*.renamed"):
         OG_name = alignment.split("/")[-1].split(".fa")[0] #Getting OG name
         print(f"Running gene IQtree for {OG_name}")
-        #Running IQtree on command line
-        unix(f"iqtree -s {alignment} -te {species_tree} --prefix {OG_name}", shell=True) 
+        #Running IQtree on command line (-keep-indent used to keep identical sequences)
+        unix(f"iqtree -s {alignment} -te {species_tree} --prefix {OG_name} -keep-ident", shell=True) 
+
+#This fucntion simply concatonates all the gene trees into an appropraite RER files
+def create_RER_input(gene_tree_files):
+    with open("combined_genetrees.txt", "w") as outf:
+        for tree in glob.glob(gene_tree_files + "*.treefile"):
+            with open(tree) as f:
+                OG_name = tree.split("/")[-1].split(".treefile")[0]
+                for line in f:
+                    tree_data = line.rstrip()
+
+                    outf.write(f"{OG_name}\t{tree_data}\n")
+
+
+#From here, script is looking to create a binary tree file for each OG 
+#These are needed for binary trait analysis in RER converge 
+#Each Binary tree file contains a binary tree with 1s for species with high GC3 
+#And 0s for species with lower GC3
+
+#First fucntion uses RegEx to replace each tree branch length value with a 0 
+def get_binary_tree_skeleton(combined_genetree_path):
+    with open(f"{combined_genetree_path}") as f:
+        #Get first tree from cmbined gene-tree output
+        first_line = f.readline()
+        tree = first_line.split("\t")[1]
+        #Match to every decimal place and replace with 0 
+        new_tree = re.sub("\d+\.\d+", "0", tree)
+        #Replace residual numbers 
+        new_tree = re.sub("\d", "0", new_tree)
+
+    return str(new_tree) 
+
+#Dictionary of each OG with list of species it is high GC3 >95% in 
+def get_highGC3_OG_dict(tsv, OG_lst):
+    highGC3_sp_OG_dict = defaultdict(list)
+    with open(tsv) as f: 
+        for line in f:
+            matched_OG = line.split("\t")[0]
+            GC3 = line.split("\t")[4]
+            sp_name = line.split("\t")[1]
+            for OG in OG_lst:
+                if (OG == matched_OG) and (float(GC3) > 95):
+                    highGC3_sp_OG_dict[OG].append(sp_name) 
+    
+    return highGC3_sp_OG_dict
+
+#Getting all binary trees
+def get_all_binary_trees(tree_skeleton, highGC3_sp_OG_dict):  
+    for OG, species in highGC3_sp_OG_dict.items():
+        with open(f"{OG}.binarytree", "w") as outf: #Open new binary tree file for each OG
+            binary_tree = tree_skeleton #Reset binary tree to skeleton
+            for sp in species: #For each high GC3 species
+                
+                binary_tree = binary_tree.replace(f"{sp}:0", f"{sp}:1")  #Replace 0 with one
+
+            outf.write(binary_tree)
 
 def main():
-    #Parse tsv and copy files
+    #Parse tsv to get list of relevant OGS
     OG_lst = parse_tsv(args.path)
-    move_files(OG_lst, args.out)
+    move_files(OG_lst, args.out) #copy files to new directory
     print(f"Copied {len(OG_lst)} OGs to {args.out}")  
     #Running trimal
     print(f"Running trimal (gappyout) for {len(OG_lst)} OGs...")
     run_trimal(args.out) 
+    
     #Currently, trimal files have gene ID as headers but species tree has species names as headers 
     #However, in order to run gene tree, need to match gene tree headers to species tree headers
     #Therefore next two fucntions get dict of gene_ID : sp_name and use it to re-write alignemts as .rename
     ID_sp_dict = get_OG_sp_dict(args.path)
     rename_trimal_files(args.out, ID_sp_dict)
     #Once files are appropriatley names, able to runIQ tree for each gene from the command line 
+    #Tree is run with constraint to match toplology of the species tree for RER analysis 
     run_gene_iqtree(args.out, args.species_tree)
+    #Then, must combine all these gene trees in tsv file for RER analysis 
+    #File in format
+    #OG_name \t tree_data
+    print("Combining trees into file combined_genetrees.txt")
+    current_path = f"{os.getcwd()}/" #IQTREE outputs to current dir so must get current path for create_RER_input
+    create_RER_input(current_path)
+   
+    # RER requires "binary trees" for binary trait calculations 
+    # Therefore the rest of this scrip will now look to etting all 
+    # binary trees for RER analysis
+    
+    #Firstly, get a tree "skeleton" with same topology as gene tree 
+    tree_skeleton = get_binary_tree_skeleton(f"combined_genetrees.txt")
+    #Get a list of high GC3 genes from tsv file 
+    OG_lst = parse_tsv(args.path)
+    #Make dictionary of each high GC3 OG : list of species in which gene is high GC
+    highGC3_OG_dict = get_highGC3_OG_dict(args.path, OG_lst)
+    #Main function 
+    get_all_binary_trees(tree_skeleton, highGC3_OG_dict)
+
 
 if __name__ == "__main__":
     main()
